585671eac561d0ba2e6b96ccc8f668a7b0650722
Pâmela Leles <pamela_leles_sousa@outlook.com>
2023-09-05 13:48:54 -0300

    First commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, StyleSheet, Text, StatusBar } from 'react-native'
+
+export default function Header(){
+  return(
+    <View>
+      <Text> Header do App</Text>
+    </View>
+  )
+}
