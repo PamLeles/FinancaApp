927b3bcd787b2cbe17c22c75e83ec1512e28da2f
Pâmela Leles <pamela_leles_sousa@outlook.com>
2023-09-14 18:17:30 -0300

    Create animation for balancing area

--- src/components/Balance/index.jsx
+++ src/components/Balance/index.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { StyleSheet } from "react-native";
+
+import { MotiView } from "moti";
 
 import {
   Container,
@@ -12,7 +15,22 @@ import {
 
 const Balance = ({ saldo, gastos }) => {
   return (
-    <Container>
+    <MotiView
+      style={styles.container}
+      from={{
+        rotateX: "-100deg",
+        opacity: 0,
+      }}
+      animate={{
+        rotateX: "-0deg",
+        opacity: 1,
+      }}
+      transition={{
+        type: "timing",
+        delay: 300,
+        duration: 900,
+      }}
+    >
       <Item>
         <ItemTitle> Saldo</ItemTitle>
         <Content>
@@ -28,8 +46,25 @@ const Balance = ({ saldo, gastos }) => {
           <Expenses>{gastos}</Expenses>
         </Content>
       </Item>
-    </Container>
+    </MotiView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingLeft: 18,
+    paddingRight: 18,
+    marginTop: -24,
+    marginLeft: 14,
+    marginRight: 14,
+    borderRadius: 4,
+    paddingTop: 22,
+    paddingBottom: 22,
+    zIndex: 99,
+  },
+});
 
 export default Balance;
